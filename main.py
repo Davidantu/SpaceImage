ca1fd199cb9a58d32bb2c0eca8cf0c605a2137fb
David Antunez <antunezd29@hotmail.com>
2020-10-26 14:14:50 -0400

    Updates code to work for all image types

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from tkinter import *
 from tkinter.ttk import *
 from urllib.request import urlopen
 import base64
+import io
+from PIL import Image, ImageTk
+
 
 class Space:
 
-    def creategui(title, image, explanation, date):
+    def creategui(title, image, explanation, date, extension):
         """Creates a GUI that displays the image for your birthday"""
         root = Tk()
 
@@ -19,22 +22,31 @@ class Space:
         y = 100
         root.geometry("%dx%d+%d+%d" % (w, h, x, y))
 
-        #get the image
-        imageX = urlopen(image).read()
-        image64 = base64.encodebytes(imageX)
-        photo = PhotoImage(data=image64)
+        if extension == 'gif':
+            # get the image for gifs
+            imageX = urlopen(image).read()
+            image64 = base64.encodebytes(imageX)
+            photo = PhotoImage(data=image64)
 
-        #put the image onto a canvas
-        cv = Canvas(bg='white')
-        cv.pack(side='top', fill='both', expand='yes')
-        cv.create_image(10, 10, image=photo, anchor='nw')
-        mainloop()
-
-
+            label = Label(root, image=photo)
+            label.pack(padx=5, pady=5)
+            mainloop()
+        else:
+            imageX = urlopen(image)
+            # create an image file object
+            imageF = io.BytesIO(imageX.read())
+            # use PIL to open image formats like .jpg  .png  .gif  etc.
+            pil_img = Image.open(imageF)
+            # convert to an image Tkinter can use
+            tk_img = ImageTk.PhotoImage(pil_img)
+            # put the image on a typical widget
+            label = Label(root, image=tk_img)
+            label.pack(padx=5, pady=5)
+            mainloop()
 
     # making requests to APOD API
     url = "https://api.nasa.gov/planetary/apod"
-    params = {'date': '1997-10-29', 'api_key': '9nXZwpSBtQ5xCr6qDfGEZLRTUC2gh3jdkZpCFYlS'}
+    params = {'date': '1997-10-29', 'api_key': '9nXZwpSBtQ5xCr6qDfGEZLRTUC2gh3jdkZpCFYlS'}#'DEMO_KEY'}
     response = requests.get(url, params)
     # create a python dictionary to be able to access specific responses
     image_info = response.json()
@@ -42,7 +54,9 @@ class Space:
     title = image_info['title']
     image = image_info['hdurl']
     explanation = image_info['explanation']
-    creategui(title, image, explanation, date)
+    # variable to hold the file extension to determine
+    extension = image[-3:]
+    creategui(title, image, explanation, date, extension)
 
 
 
